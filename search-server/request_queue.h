4a08173ae696f9cfaca02a2aed704c0f1b3ecff6
aksi321 <96183002+aksi321@users.noreply.github.com>
2024-11-09 15:31:19 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -8,23 +8,11 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
 
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        AddRequest(result.size());
-        return result;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
 
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) {
-        const auto result = search_server_.FindTopDocuments(raw_query, status);
-        AddRequest(result.size());
-        return result;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
-    std::vector<Document> AddFindRequest(const std::string& raw_query) {
-        const auto result = search_server_.FindTopDocuments(raw_query);
-        AddRequest(result.size());
-        return result;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
 
     int GetNoResultRequests() const;
 
@@ -41,3 +29,10 @@ private:
 
     void AddRequest(int results_num);
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    AddRequest(result.size());
+    return result;
+}
